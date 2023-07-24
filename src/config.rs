227a77ee860782cb92fda57d1a643de2b43e2dde
Zola Gonano <zolagonano@protonmail.com>
2023-07-24 13:49:59 +0000

    Add docs for config.rs

--- src/config.rs
+++ src/config.rs
@@ -2,14 +2,22 @@ use config::Config;
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
+/// Struct representing the configuration file for the userbot.
 #[derive(Serialize, Deserialize, Clone)]
 pub struct ConfigFile {
+    /// Token for the userbot's Telegram bot.
     pub bot_token: String,
+    /// Address of the server where the userbot is hosted.
     pub server_address: String,
+    /// List of ports used by the userbot.
     pub ports: Vec<u32>,
+    /// Location information of the userbot.
     pub location: String,
+    /// List of user IDs designated as administrators.
     pub admin_list: Vec<i64>,
+    /// ID of the chat used for logging.
     pub log_chat: i64,
+    /// Prefix used for userbot commands.
     pub prefix: String,
 }
 
@@ -24,6 +32,7 @@ impl fmt::Display for ConfigFile {
 }
 
 impl ConfigFile {
+    /// Loads the configuration from the specified file path and returns a `ConfigFile` instance.
     pub fn load() -> Result<ConfigFile, Box<dyn std::error::Error>> {
         let settings = Config::builder()
             .add_source(config::File::with_name("/etc/userbot.json"))
